90f9c3d7a74e1960b0805b3f02daaf1dda6fdd45
Hjaehong <zero5140@naver.com>
2023-03-22 17:50:24 +0900

    action test

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux';
-import addBookReducer from './AddBookModule';
 
 const rootReducer = combineReducers({
-    addBookReducer
 });
 
 export default rootReducer;
