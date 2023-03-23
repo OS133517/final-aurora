966f5f5e89ae09fb3c2f9a3723cbf5ad664e86e4
OS133517 <115386472+OS133517@users.noreply.github.com>
2023-03-23 13:48:39 +0900

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,7 @@ function Header() {
 
     return (
         <div className={HeaderCSS.header}>
-            <img src={ `${process.env.PUBLIC_URL}aurora.png` } alt="메인로고"/>
+            <img src={ `${process.env.PUBLIC_URL}/aurora.png` } alt="메인로고"/>
         </div>
     )
 }
