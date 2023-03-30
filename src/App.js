93b770892e90e39355b2a2c47fc470a65d9f0def
osj <os133517@gmail.com>
2023-03-30 20:25:07 +0900

    # css 수정

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import Addresses from "./pages/addBook/Addresses";
 import Main from "./pages/Main";
 import MyReservation from "./pages/reservation/MyReservation";
 import ReservationCalendarLayout from "./layouts/ReservationCalendarLayout";
+import "./App.css";
 
 function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/">
-        <Route index element={<Login/>}/>
-        <Route path="aurora" element={<Layout/>}>
-          <Route index element={<Main/>}/>
-          <Route path="address-book" element={<AddBookLayout/>}>
-            <Route index element={<Addresses/>}/>
-            <Route path="addresses" element={<Addresses category='전체 주소록'/>}/>
-            <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록'/>}/>
-            <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록'/>}/>
-          </Route>
-          <Route path="reservation" element={<ReservationLayout/>}>
-            {/* <Route index element={<MyReservation/>}/> */}
-            <Route path="my-reservation" element={<MyReservation/>}/>
-            <Route path="assets/:assetCode" element={<ReservationCalendarLayout/>}/>
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Login/>}/>
+          <Route path="aurora" element={<Layout/>}>
+            <Route index element={<Main/>}/>
+            <Route path="address-book" element={<AddBookLayout/>}>
+              <Route index element={<Addresses/>}/>
+              <Route path="addresses" element={<Addresses category='전체 주소록'/>}/>
+              <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록'/>}/>
+              <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록'/>}/>
+            </Route>
+            <Route path="reservation" element={<ReservationLayout/>}>
+              {/* <Route index element={<MyReservation/>}/> */}
+              <Route path="my-reservation" element={<MyReservation/>}/>
+              <Route path="assets/:assetCode" element={<ReservationCalendarLayout/>}/>
+            </Route>
           </Route>
         </Route>
-      </Route>
-    </Routes>
-   </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
