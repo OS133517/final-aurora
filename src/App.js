727fef813a2ac5d48155e2945c580c92139921da
Hjaehong <zero5140@naver.com>
2023-03-31 16:08:19 +0900

    0331

--- src/App.js
+++ src/App.js
@@ -19,35 +19,37 @@ import DraftForm from "./pages/approval/DraftForm";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Login />} />
-          <Route path="aurora" element={<Layout />}>
-            <Route index element={<Main />} />
-            <Route path="address-book" element={<AddBookLayout />}>
-              <Route index element={<Addresses />} />
-              <Route path="addresses" element={<Addresses category='전체 주소록' />} />
-              <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록' />} />
-              <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록' />} />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Login />} />
+            <Route path="aurora" element={<Layout />}>
+              <Route index element={<Main />} />
+              <Route path="address-book" element={<AddBookLayout />}>
+                <Route index element={<Addresses />} />
+                <Route path="addresses" element={<Addresses category='전체 주소록' />} />
+                <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록' />} />
+                <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록' />} />
+              </Route>
+              <Route path="reservation" element={<ReservationLayout />}>
+                {/* <Route index element={<MyReservation/>}/> */}
+                <Route path="my-reservation" element={<MyReservation />} />
+                <Route path="assets/:assetCode" element={<ReservationCalendarLayout />} />
+              </Route>
+              <Route path="approval" element={<ApprovalLayout />}>
+                <Route index="approvals" element={<Approvals />} />
+                <Route index path="pending" element={<Pending />} />
+                <Route index path="draft" element={<ApprovalDraft />} />
+                <Route index path="detail/:appCode" element={<ApprovalDetail />} />
+                <Route index path="form/:docCode" element={<DraftForm />} />
+              </Route>
             </Route>
-            <Route path="reservation" element={<ReservationLayout />}>
-              <Route index element={<MyReservation />} />
-              <Route path="my-reservation" element={<MyReservation />} />
-            </Route>
-            <Route path="approval" element={<ApprovalLayout />}>
-              <Route index="approvals" element={<Approvals />} />
-              <Route index path="pending" element={<Pending />} />
-              <Route index path="draft" element={<ApprovalDraft />} />
-              <Route index path="detail/:appCode" element={<ApprovalDetail />} />
-              <Route index path="form/:docCode" element={<DraftForm />} />
-            </Route>
-          </Route>
 
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div >
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
