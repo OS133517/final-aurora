62310292ac80b893071ede981e9498a2db3b5022
osj <os133517@gmail.com>
2023-04-10 00:56:25 +0900

    머지중

--- src/App.js
+++ src/App.js
@@ -14,10 +14,8 @@ import ApprovalWait from "./pages/approval/ApprovalWait";
 
 import AddBookLayout from "./layouts/AddBookLayout";
 import Addresses from "./pages/addBook/Addresses";
-import ApprovalDetail from "./pages/approval/ApprovalDetail";
 
 import ReservationLayout from "./layouts/ReservationLayout";
-import Login from "./pages/Login";
 import MyReservation from "./pages/reservation/MyReservation";
 import ReservationCalendar from "./pages/reservation/ReservationCalendar";
 import ReservationAssetManagement from "./pages/reservation/ReservationAssetManagement";
@@ -39,14 +37,6 @@ import CasualReports from "./pages/report/CasualReports";
 import CasualReportDetail from "./pages/report/CasualReportDetail";
 import ReportRounds from "./pages/report/ReportRounds";
 import ReportRoundDetail from "./pages/report/ReportRoundDetail";
-import MyReservation from "./pages/reservation/MyReservation";
-import ReservationCalendar from "./pages/reservation/ReservationCalendar";
-import "./App.css";
-import ApprovalDraft from "./pages/approval/ApprovalDraft";
-import DraftForm from "./pages/approval/DraftForm";
-
-import HrmDetail from './pages/hrm/HrmDetail';
-import HrmModify from './pages/hrm/HrmModify';
 
 import WorklogLayout from "./layouts/WorklogLayout";
 import DayWorklogs from "./pages/worklog/DayWorklogs";
@@ -118,61 +108,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
-<<<<<<<<< Temporary merge branch 1
-=========
-   <BrowserRouter>
-    <Routes>
-<<<<<<<<< Temporary merge branch 1
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Main/>}/>
-        <Route path="address-book" element={<AddBookLayout/>}>
-              <Route index element={<Addresses/>}/>
-              <Route path="addresses" element={<Addresses category='전체 주소록'/>}/>
-              <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록'/>}/>
-              <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록'/>}/>
-            </Route>
-<<<<<<<<< Temporary merge branch 1
-
-        <Route path="worklog" element={<WorklogLayout/>}>
-          <Route path="day" element={<DayWorklogs/>}/>
-          <Route path="day/:dayWorklogCode" element={<DayWorklogDetail/>}/>
-          <Route path="day/insert" element={<DayWorklogInsert/>}/>
-
-=========
-      <Route path="/">
-        <Route index element={<Login/>}/>
-        <Route path="aurora" element={<Layout/>}>
-          <Route index element={<Main/>}/>
-          <Route path="address-book" element={<AddBookLayout/>}>
-            <Route index element={<Addresses/>}/>
-            <Route path="addresses" element={<Addresses category='전체 주소록'/>}/>
-            <Route path="personal-groups/:groupCode" element={<Addresses category='개인 주소록'/>}/>
-            <Route path="team-groups/:groupCode" element={<Addresses category='공용 주소록'/>}/>
-          </Route>
-          <Route path="hrm" element={<HrmLayout/>}>
-            <Route index element={<Hrm/>}/>
-            <Route path="list" element={<Hrm category='인사 목록'/>}/>
-            <Route path="hrm-detail/:memberCode" element={<HrmDetail category='인사 정보'/>}/>
-            <Route path="hrm-modify" element={<Hrm category='인사 수정'/>}/>
-            <Route path="hrm-modify/:memberCode" element={<HrmModify category='인사 수정'/>}/>
-=========
-            <Route path="reservation" element={<ReservationLayout/>}>
-              <Route index element={<MyReservation/>}/>
-              <Route path="my-reservation" element={<MyReservation/>}/>
-              <Route path="assets/:assetCode" element={<ReservationCalendar/>}/>
-            </Route>
->>>>>>>>> Temporary merge branch 2
-          </Route>
-          
->>>>>>>>> Temporary merge branch 2
-        </Route>
-      
-      </Route>
-    </Routes>
-   </BrowserRouter>
->>>>>>>>> Temporary merge branch 2
-=========
->>>>>>>>> Temporary merge branch 2
   );
 }
 
