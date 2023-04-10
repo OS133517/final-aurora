88be1d5a0f0dbf8b32271c928e4fbd56bf1bac3c
root <jgh@gmail.com>
2023-04-10 09:34:49 +0900

    임시 커밋

--- src/pages/attendance/VacationDetail.js
+++ src/pages/attendance/VacationDetail.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import VacationDetailCSS from "./VacationDetail.module.css"
 
 export default function VacationDetail() {
     return (
-        <div>
-            d
+
+    
+        <div className={VacationDetailCSS.vacationDiv}>
+            <div className={VacationDetailCSS.vacationHeader}>
+                <span>휴가 상세</span>
+            </div>
+            <div className={VacationDetailCSS.topContainer}>
+                <div className={VacationDetailCSS.topContentWrapper}>
+                    <span className={VacationDetailCSS.topContentTitle}>휴가관리</span>
+                <div className={VacationDetailCSS.topContent}>
+                    
+                
+                    <div className={VacationDetailCSS.topBox}>
+                        <div className={VacationDetailCSS.inBox}>
+
+                        </div>
+                        <div className={VacationDetailCSS.inBox}>
+
+                        </div>
+                        <div className={VacationDetailCSS.inBox}>
+
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                <div className={VacationDetailCSS.buttonWrapper}>
+                     <button className={VacationDetailCSS.VacationButton} type="">휴가신청</button>
+                </div>
+                </div>
+            </div>
+            <div className={VacationDetailCSS.lowContainer}>
+
+            </div>
         </div>
+    
     );
 }
 
